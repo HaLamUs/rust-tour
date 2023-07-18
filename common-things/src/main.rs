f18e6278e8a554dd894bc07b222ab855762d9ec9
HaLamUs <itzlamz9x@gmail.com>
2023-07-18 22:47:58 +0700

    feat: shadowing

--- common-things/src/main.rs
+++ common-things/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    let mut x = 5;
+    let x = 5;
     println!("This is the value of {}", x);
-    x = 6;
+    let x = 6.2;
     print!("This is the value of {}", x);
 
     const MY_BANK_NUMBER: u32 = 100_000;
