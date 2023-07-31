6d43f871b128d5de4d0a4e55a0fbb9a1956a8af0
HaLamUs <itzlamz9x@gmail.com>
2023-07-31 23:18:27 +0700

    feat: FAI if-let

--- rust-enum/src/main.rs
+++ rust-enum/src/main.rs
@@ -1,13 +1,12 @@
-fn plus_one(x: Option<i32>) -> Option<i32> {
-  match x {
-      // None => None,
-      _ => None,
-      Some(i) => Some(i +1), // need wrap return in Some 
-  }
-}
 
 fn main() {
-  let five = Some(5);
-  let six = plus_one(five);
-  let none = plus_one(None);
+  let some_value = Some(3);
+  match some_value {
+    Some(3) => println!("Three"),
+    _ => ()
+  }
+
+  if let Some(3) = some_value {
+    println!("Three");
+  }
 }
