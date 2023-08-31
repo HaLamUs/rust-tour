1a202db7bdb7a58e4152803a13572be013b2b16e
HaLamUs <itzlamz9x@gmail.com>
2023-08-31 22:52:25 +0700

    feat: RC in short

--- reference-counting/README.md
+++ reference-counting/README.md
@@ -90,6 +90,25 @@ let b = Cons(3, a.clone());
 
 ```
 
+## Counting 
+```Rust
+fn main() {
+  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc:new(Nil)))));
+  println!("Count after creating a = {}", Rc::strong_count(&a)); // 1
+  let b = Cons(3, Rc::clone(&a));
+  println!("Count after creating b = {}", Rc::strong_count(&a)); // 2
+  {
+    let c = Cons(4, Rc::clone(&a));
+    println!("Count after creating c = {}", Rc::strong_count(&a)); // 3
+  }
+  println!("Count after c goes out of scope {}", Rc::strong_count(&a)); // 2
+}
+
+
+```
+
+## Sumary 
+RC only allows multiple parts of ur program to read the same data not modify it 
 
 
 
--- reference-counting/src/main.rs
+++ reference-counting/src/main.rs
@@ -4,14 +4,20 @@ use std::rc::Rc;
 
 enum List {
   Cons(i32, Rc<List>),
-  Nil
+  Nil,
 }
 
 
 use crate::List::{Cons, Nil};
 
 fn main() {
-  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc:new(Nil)))));
-  let b = Cons(3, a.clone()); // same as below 
-  let c = Cons(4, Rc::clone(&a));
+  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+  println!("Count after creating a = {}", Rc::strong_count(&a)); // 1
+  let b = Cons(3, Rc::clone(&a));
+  println!("Count after creating b = {}", Rc::strong_count(&a)); // 2
+  {
+    let c = Cons(4, Rc::clone(&a));
+    println!("Count after creating c = {}", Rc::strong_count(&a)); // 3
+  }
+  println!("Count after c goes out of scope {}", Rc::strong_count(&a)); // 2
 }
